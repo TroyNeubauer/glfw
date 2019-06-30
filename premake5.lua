20fcd0cdb37281e380ebdbcb99798d9f3f926471
Troy Neubauer <troyneubauer@gmail.com>
2019-06-30 15:41:47 -0400

    Added unix premake file

--- premake5.lua
+++ premake5.lua
@@ -10,7 +10,6 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
@@ -19,8 +18,11 @@ project "GLFW"
 		"src/window.c"
 	}
 	
+	defines "_GLFW_VULKAN_STATIC"
+
+	systemversion "latest"
+	
 	filter "system:windows"
-		systemversion "latest"
 		
 		files
 		{
@@ -40,6 +42,28 @@ project "GLFW"
 			"_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
 		}
+		
+		
+	filter "system:linux"
+		
+		files
+		{
+			"src/x11_init.c",
+			"src/linux_joystick.c",
+			"src/x11_monitor.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/x11_window.c",
+			"src/glx_context.c",
+		}
+
+		defines 
+		{ 
+			"_GLFW_X11",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+		
+		
 
 	filter "configurations:Debug"
 		runtime "Debug"
